a595efb6823ab8346287e627581215a13c9ec6c0
Musa Muhammad <musaawwaletudaye@gmail.com>
2023-01-26 23:54:47 +0100

    feat: made some improvement on the SecSubPage contents

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <div className="flex bg-HomeBackground w-full h-[60px] justify-between sm:justify-between p-11 items-center">
       <div className="flex justify-center items-center">
-        <CgMenuRound color="#c7ffca" size={50} />
+        <CgMenuRound color="#c7ffca" size={50.01} />
         <p className="font-sans text-[2rem] text-AppGreen pl-3">neoplants</p>
       </div>
       <div className="w-[11rem] h-[3rem] bg-AppGreen rounded-3xl flex items-center justify-center hover:bg:">
